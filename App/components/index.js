8105f5af5a5a52fa0a405be160048d225efc660f
goldstar0415 <topdeveloper1212@gmail.com>
2016-08-20 14:15:25 -0500

    Images to holder data

--- App/components/index.js
+++ App/components/index.js
@@ -8,19 +8,63 @@ import React, { Component } from 'react';
 import {
 
   StyleSheet,
+  Image,
   Text,
   View
 } from 'react-native';
 import Nav from './global-widgets/nav'
 import SwipeCards from 'react-native-swipe-cards';
-const Cards = [
-  {text: 'Tomato', backgroundColor: 'red'},
-  {text: 'Aubergine', backgroundColor: 'purple'},
-  {text: 'Courgette', backgroundColor: 'green'},
-  {text: 'Blueberry', backgroundColor: 'blue'},
-  {text: 'Umm...', backgroundColor: 'cyan'},
-  {text: 'orange', backgroundColor: 'orange'},
-]
+
+var image1 = require('../images/image1.jpeg')
+var image2 = require('../images/image2.jpeg')
+var image3 = require('../images/image3.jpeg')
+var image4 = require('../images/image4.jpeg')
+var image5 = require('../images/image5.jpeg')
+var image6 = require('../images/image6.jpeg')
+
+const Cards = [{
+  "id": 1,
+  "first_name": "Denise",
+  "age": 21,
+  "friends": 9,
+  "interests": 38,
+  "image": image1
+}, {
+  "id": 2,
+  "first_name": "Cynthia",
+  "age": 27,
+  "friends": 16,
+  "interests": 49,
+  "image": image2
+}, {
+  "id": 3,
+  "first_name": "Maria",
+  "age": 29,
+  "friends": 2,
+  "interests": 39,
+  "image": image3
+}, {
+  "id": 4,
+  "first_name": "Jessica",
+  "age": 20,
+  "friends": 18,
+  "interests": 50,
+  "image": image4
+}, {
+  "id": 5,
+  "first_name": "Julie",
+  "age": 28,
+  "friends": 2,
+  "interests": 13,
+  "image": image5
+}, {
+  "id": 6,
+  "first_name": "Anna",
+  "age": 24,
+  "friends": 12,
+  "interests": 44,
+  "image": image6
+}]
 
 export default class Index extends Component {
   constructor(props){
@@ -31,8 +75,8 @@ export default class Index extends Component {
   }
   Card(x){
     return (
-      <View style={[styles.card, {backgroundColor: x.backgroundColor}]}>
-        <Text>{x.text}</Text>
+      <View style={styles.card}>
+        <Image source ={image1} resizeMode="contain" style ={{width:350, height:350}} />
       </View>
     )
   }
@@ -75,10 +119,12 @@ const styles = StyleSheet.create({
   },
    card: {
     flex: 1,
-    justifyContent: 'center',
     alignItems: 'center',
-    width: 300,
-    height: 300,
+    alignSelf:'center',
+    borderWidth:2,
+    borderColor:'#e3e3e3',
+    width: 350,
+    height: 400,
   }
  
 });
