ee0befba5ebad62604e8e4c4ca2ab840d101385d
goldstar0415 <topdeveloper1212@gmail.com>
2016-08-21 13:38:44 -0500

    Navigation icons

--- App/components/global-widgets/nav.js
+++ App/components/global-widgets/nav.js
@@ -13,14 +13,15 @@ import {
 } from 'react-native';
 
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import Iconz from 'react-native-vector-icons/Ionicons';
 
 export default class Nav extends Component {
   render() {
     return (
       <View style={styles.container}>
-      <Icon name="menu" color ="#333" size={20} style={{margin:10}} />
+      <Iconz name="ios-settings" color ="#888" size={20} style={{margin:10}} />
       <Image source ={require('../../images/logo.png')} resizeMode = "contain" style={{width:100, height:30}} />
-      <Icon name="forum" color ="#555" size={25} style={{margin:10}} />
+      <Iconz name="ios-chatboxes-outline" color ="#555" size={25} style={{margin:10}} />
       </View>
     );
   }
