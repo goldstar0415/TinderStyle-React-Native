3f7a0ce7578764eb03c46dd90e1e415c84f23b41
goldstar0415 <topdeveloper1212@gmail.com>
2016-08-21 13:47:50 -0500

    Home.js Exists now

---
+++ App/components/messages.js
@@ -0,0 +1,76 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+
+  StyleSheet,
+  Image,
+  Text,
+  TouchableOpacity,
+  View
+} from 'react-native';
+import Nav from './global-widgets/nav'
+import SwipeCards from 'react-native-swipe-cards';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import Iconz from 'react-native-vector-icons/Ionicons';
+
+
+export default class Index extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      cards: Cards
+    }
+  }
+  
+   
+  render() {
+    return (
+      <View style={styles.container}>
+      <Nav />
+     
+        </View>
+    )
+}
+}
+//onPress = {() => this.renderNope()} 
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    backgroundColor: '#f7f7f7',
+  },
+  buttons:{
+    width:80, 
+    height:80, 
+    borderWidth:10, 
+    borderColor:'#e7e7e7', 
+    justifyContent:'center', 
+    alignItems:'center',
+    borderRadius:40
+  },
+  buttonSmall:{
+    width:50, 
+    height:50, 
+    borderWidth:10, 
+    borderColor:'#e7e7e7', 
+    justifyContent:'center', 
+    alignItems:'center',
+    borderRadius:25
+  },
+   card: {
+    flex: 1,
+    alignItems: 'center',
+    alignSelf:'center',
+    borderWidth:2,
+    borderColor:'#e3e3e3',
+    width: 350,
+    height: 420,
+  }
+ 
+});
